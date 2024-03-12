54415c488d5eb54b8a1d85a66b5e505312bd907a
jesfre <jorge.ruiz.aquino@gmail.com>
2024-03-11 21:08:13 -0400

    Issue #4. Support for exporting Head or specific revision

--- src/main/java/com/blogspot/jesfre/svn/utils/SvnExport.java
+++ src/main/java/com/blogspot/jesfre/svn/utils/SvnExport.java
@@ -9,16 +9,33 @@ public class SvnExport {
 	private boolean verbose;
 
 	public SvnExport verbose(boolean yesNo) {
-	this.verbose = yesNo;
-	return this;
+		this.verbose = yesNo;
+		return this;
 	}
 
 	/**
+	 * Exports the given revision of the given file
+	 * @param revision
 	 * @param svnManagedFile
 	 * @param exportOutputFile
 	 * @return the path to the exported file
 	 */
-	public String export(String svnManagedFile, String exportOutputFile) {
+	public String export(long revision, String svnManagedFile, String exportOutputFile) {
+		String cmdExportCommand = getCommand(revision, svnManagedFile, exportOutputFile);
+
+		CommandLineRunner runner = new CommandLineRunner();
+		runner.setVerbose(verbose);
+		runner.executeCommand(cmdExportCommand);
+		return exportOutputFile;
+	}
+
+	/**
+	 * Exports the HEAD revision of the given file
+	 * @param svnManagedFile
+	 * @param exportOutputFile
+	 * @return the path to the exported file
+	 */
+	public String exportHead(String svnManagedFile, String exportOutputFile) {
 		String cmdExportCommand = getCommand(svnManagedFile, exportOutputFile);
 
 		CommandLineRunner runner = new CommandLineRunner();
@@ -27,9 +44,29 @@ public class SvnExport {
 		return exportOutputFile;
 	}
 
+	/**
+	 * Gets the command to export the HEAD revision
+	 * @param svnManagedFile
+	 * @param exportOutputFile
+	 * @return
+	 */
 	public String getCommand(String svnManagedFile, String exportOutputFile) {
 		String cmdExportCommand = StringUtils.replace(SvnConstants.SVN_EXPORT_CMD_TEMPLATE, "URL_FILE", svnManagedFile);
 		cmdExportCommand = StringUtils.replace(cmdExportCommand, "EXPORTED_JAVA_FILEPATH", exportOutputFile);
+		return cmdExportCommand;
+	}
+
+	/**
+	 * Gets the command to export the given revision
+	 * @param revision
+	 * @param svnManagedFile
+	 * @param exportOutputFile
+	 * @return
+	 */
+	public String getCommand(long revision, String svnManagedFile, String exportOutputFile) {
+		String cmdExportCommand = StringUtils.replace(SvnConstants.SVN_EXPORT_REV_CMD_TEMPLATE, "URL_FILE", svnManagedFile);
+		cmdExportCommand = StringUtils.replace(cmdExportCommand, "EXPORTED_JAVA_FILEPATH", exportOutputFile);
+		cmdExportCommand = StringUtils.replace(cmdExportCommand, "REVISION", String.valueOf(revision));
 		return cmdExportCommand;
 	}
 }
